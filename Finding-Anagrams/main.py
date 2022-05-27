f74e01cb84f71906157ee9213c776b319f7d4db9
oyeniran samuel sanmi <sossesam@gmail.com>
2022-05-27 09:52:20 +0100

    Add files via upload

---
+++ Finding-Anagrams/main.py
@@ -0,0 +1,36 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    first_word = list(word)
+    second_word =list(anagram)
+    word_check = []
+
+    
+
+    for letter in first_word:
+        if letter in second_word:
+            word_check.append(letter)
+
+    if (len(first_word) == len(word_check)):
+        print("True")
+    else:
+        print("False")
+
+
+
+    
+
+
+    
+
+    
+
+    
+
+find_anagram("hello", "check")
